2a40361eb0c9f130231e674ef2a2caf1a8cc05a7
AvinFajarF <avinfajar6@gmail.com>
2024-04-10 21:57:28 +0700

    feat: added relationship model

--- app/Models/Discussions.php
+++ app/Models/Discussions.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Discussions extends Model
 {
     use HasFactory;
+
+    public function supervisor() : BelongsTo
+    {
+        return $this->belongsTo(Supervisors::class);
+    }
+
+    public function comments() : BelongsTo
+    {
+        return $this->belongsTo(DiscussionComments::class);
+    }
 }
--- app/Models/Supervisors.php
+++ app/Models/Supervisors.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Supervisors extends Model
 {
     use HasFactory;
+
+    public function discussions() : BelongsTo
+    {
+        return $this->belongsTo(Discussions::class);
+    }
 }
